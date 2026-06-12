3643ca727498627b33ff859ef240de5b97c5b840
TacticusFinch <nagajolokias@gmail.com>
2026-06-13 01:53:49 +0800

    Калибрую время по Варшаве 4

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1491,7 +1491,7 @@ async def handle_user_homework_answer(message: types.Message, state: FSMContext)
 def schedule_daily_tests(bot: Bot):
     """ Планирует отправку тестов каждый день """
     scheduler.start()
-    scheduler.add_job(start_new_test_for_all, "cron", hour="17", minute=53, args=[bot])
+    scheduler.add_job(start_new_test_for_all, "cron", hour="17", minute=55, args=[bot])
     #scheduler.add_job(send_homework_for_beginners, "interval", minutes=2, args=[bot])
 
 @user_private_router.message(Command('state'))
